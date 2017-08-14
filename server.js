8ea2feb45584557df2e38b2a7725127bcf95a3e4
unnatiaggarwalsmile <30660106+unnatiaggarwalsmile@users.noreply.github.com>
2017-08-14 20:17:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one',function(req,res){
+    res.send('article one is demanded and it will be served here');
+});
+    
+    app.get('/article-two',function(req,res){
+    res.send('article two is demanded and it will be served here');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('article three is demanded and it will be served here');
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
