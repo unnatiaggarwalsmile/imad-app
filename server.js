708eb2a2d7719880cba458e1755f7dae813026da
unnatiaggarwalsmile <30660106+unnatiaggarwalsmile@users.noreply.github.com>
2017-08-14 20:55:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req,res){
 });
     
     app.get('/article-two',function(req,res){
-    res.send('heyy guys its my first web page by imad ');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
